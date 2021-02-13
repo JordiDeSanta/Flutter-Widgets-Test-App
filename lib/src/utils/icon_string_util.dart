1fd51a520383acb860662e7b47478b1a0628922d
Jordi <jordi.bravo.bad@gmail.com>
2021-02-12 22:22:36 -0300

    Re-factorizing getIcon method

--- lib/src/utils/icon_string_util.dart
+++ lib/src/utils/icon_string_util.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 Color _iconsColor = Color.fromARGB(255, 90, 178, 218);
 
-final _icons = <String, Icon>{
-  "accessibility": Icon(Icons.accessibility, color: _iconsColor),
-  "add_alert": Icon(Icons.add_alert, color: _iconsColor),
-  "folder_open": Icon(Icons.folder_open, color: _iconsColor),
-  "default_arrow": Icon(Icons.arrow_forward_ios, color: _iconsColor),
+final _icons = <String, IconData>{
+  "accessibility": Icons.accessibility,
+  "add_alert": Icons.add_alert,
+  "folder_open": Icons.folder_open,
+  "default_arrow": Icons.arrow_forward_ios,
 };
 
 Icon getIcon(String iconName) {
-  return _icons[iconName];
+  return Icon(_icons[iconName], color: _iconsColor);
 }
