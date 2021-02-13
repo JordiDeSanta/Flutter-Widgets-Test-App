6d40c926cc95a19982dcb9c38b5d3ce61a8a2f02
Jordi <jordi.bravo.bad@gmail.com>
2021-02-13 19:10:21 -0300

    Showing image from url and assets

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 // Own
 import 'package:components/src/utils/color_palette.dart';
+import 'package:components/src/utils/links.dart';
 
 class CardPage extends StatelessWidget {
   const CardPage({Key key}) : super(key: key);
@@ -15,14 +16,14 @@ class CardPage extends StatelessWidget {
         backgroundColor: appBarColor,
       ),
       body: ListView(
-        children: [_card()],
+        children: [_cardTypeOne(), _cardTypeTwo('Air Jordan 1')],
         padding: EdgeInsets.all(10),
       ),
     );
   }
 }
 
-Widget _card() {
+Widget _cardTypeOne() {
   return Card(
     child: Column(
       children: [
@@ -37,6 +38,24 @@ Widget _card() {
             FlatButton(onPressed: () {}, child: Text("Cancel")),
           ],
           mainAxisAlignment: MainAxisAlignment.end,
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _cardTypeTwo(String linkName) {
+  return Card(
+    child: Column(
+      children: [
+
+        FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: NetworkImage(links[linkName]),
+          ),
+        Container(
+          child: Text(linkName),
+          padding: EdgeInsets.all(20.0),
         ),
       ],
     ),
