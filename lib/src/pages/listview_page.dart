5c380df19e4698b23038354cd2f6d3c25f5f3700
Jordi <jordi.bravo.bad@gmail.com>
2021-02-15 19:55:44 -0300

    infinite Scroll with Lorem Picsum images

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -10,7 +10,23 @@ class ListViewPage extends StatefulWidget {
 }
 
 class _ListViewPageState extends State<ListViewPage> {
-  List<int> _numberList = [121, 201, 123, 19];
+  ScrollController _scrollController = new ScrollController();
+  List<int> _numberList = new List();
+  int _lastItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _add10();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _add10();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +41,26 @@ class _ListViewPageState extends State<ListViewPage> {
 
   Widget _createList() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _numberList.length,
       itemBuilder: (BuildContext context, int i) {
         final _image = _numberList[i];
-        return Image.network('https://picsum.photos/1280/720/?image=$_image');
+        return FadeInImage(
+          height: 200,
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://picsum.photos/1280/720/?image=$_image'),
+          fit: BoxFit.cover,
+        );
       },
     );
+  }
+
+  void _add10() {
+    for (var i = 1; i < 10; i++) {
+      _lastItem++;
+      _numberList.add(_lastItem);
+    }
+
+    setState(() {});
   }
 }
