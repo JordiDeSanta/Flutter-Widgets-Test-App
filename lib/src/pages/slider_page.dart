1b0021b51399d69dad65425a73f85e3620dc54b6
Jordi <jordi.bravo.bad@gmail.com>
2021-02-15 17:15:38 -0300

    Slider Widget with movement

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -1,3 +1,5 @@
+// Third Party
+import 'package:components/src/utils/color_palette.dart';
 import 'package:flutter/material.dart';
 
 class SliderPage extends StatefulWidget {
@@ -8,12 +10,32 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
+  double _sliderValue = 0.5;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Slider Page'),
+        backgroundColor: appBarColor,
       ),
+      body: Column(
+        children: [
+          _createSlider(),
+        ],
+      ),
+    );
+  }
+
+  Widget _createSlider() {
+    return Slider(
+      value: _sliderValue,
+      onChanged: (alpha) {
+        setState(() {
+          _sliderValue = alpha;
+        });
+      },
+      activeColor: iconsColor,
     );
   }
 }
